c515d7d3da606ce1afbc36d861ed70f1386800f4
wxl <1341206737@qq.com>
2018-12-03 09:28:59 +0800

    完成sass编译和起服务

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: 王鑫磊 
  * @Date: 2018-12-03 08:52:04 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-03 09:11:38
+ * @Last Modified time: 2018-12-03 09:27:12
  */
 
 
@@ -10,9 +10,11 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var server = require('gulp-webserver');
 // var babel = require('gulp-babel');
 var url = require('url');
-
+var data = require('./src/json/data.json');
+var path = require('path');
 var fs = require('fs');
 
 
@@ -27,5 +29,34 @@ gulp.task('watch',function(){
     return gulp.watch('./src/scss/index.scss',gulp.series('devScss'))
 })
 
-gulp.task('dev',gulp.parallel('devScss','watch'))
+gulp.task('dev',gulp.parallel('devScss','watch'));
+
+
+//起服务
+
+gulp.task('server',function(){
+    return gulp.src('src')
+    .pipe(server({
+        port:8888,
+        open:true,
+        middleware:function(req,res,next){
+            var pathname = url.parse(req.url).pathname;
+
+            if(pathname === '/favicon.ico'){
+                res.end('')
+                return 
+            }
+
+            if(pathname === '/getDate'){
+                res.end(JSON.stringify({code:1,list:data[0]}))
+            }else{
+                pathname = pathname === '/' ? 'index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname,'src',pathname)))
+            }
+        }
+    }))
+})
+
+
+
 
